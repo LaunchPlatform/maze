f05eceb4ad84ed0338bdba244c20a447628cc6c1
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-27 22:06:50 -0800

    More tests

--- tests/gene/test_utils.py
+++ tests/gene/test_utils.py
@@ -1,5 +1,6 @@
 import pytest
 
+from maze.gene.utils import consume_int
 from maze.gene.utils import gen_bits
 
 
@@ -17,3 +18,23 @@ from maze.gene.utils import gen_bits
 )
 def test_gen_bits(data: bytes, expected: tuple[int, ...]):
     assert tuple(gen_bits(data)) == tuple(expected)
+
+
+@pytest.mark.parametrize(
+    "bits, bit_lens, expected",
+    [
+        ((1, 1, 0, 0, 0, 0, 1, 0), (2,), (3,)),
+        ((1, 1, 0, 0, 0, 0, 1, 0), (2, 1), (3, 0)),
+        ((1, 1, 0, 0, 0, 0, 1, 0), (2, 5), (3, 16)),
+        ((1, 1, 0, 0, 0, 0, 1, 0), (3,), (3,)),
+        ((1, 1, 0, 0, 0, 0, 1, 0), (7,), (67,)),
+    ],
+)
+def test_consume_int(
+    bits: tuple[int, ...], bit_lens: tuple[int, ...], expected: tuple[int, ...]
+):
+    bits_iter = iter(bits)
+    assert (
+        tuple(consume_int(bits=bits_iter, bit_len=bit_len) for bit_len in bit_lens)
+        == expected
+    )
