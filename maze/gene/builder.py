a7fbbfc5e45a563d4f87e7d7a5a4893737311cb8
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-27 22:26:18 -0800

    Handle repeat

--- maze/gene/builder.py
+++ maze/gene/builder.py
@@ -1,14 +1,27 @@
+import typing
+
 from torch import nn
 
 from .symbols import Symbol
 
 
-def build_models(symbols: list[Symbol]) -> list[nn.Module]:
+def build_models(
+    symbols: typing.Iterator[Symbol], till_symbol: Symbol | None = None
+) -> list[nn.Module]:
     modules = []
-    for item in symbols:
+    while True:
+        try:
+            item = next(symbols)
+        except StopIteration:
+            break
+        if till_symbol is not None and item == till_symbol:
+            return modules
         match item:
             case (Symbol.REPEAT_START, times):
-                pass
+                for _ in range(times):
+                    modules.extend(
+                        build_models(symbols=symbols, till_symbol=Symbol.REPEAT_END)
+                    )
             case (Symbol.LINEAR, output_features):
                 modules.append(nn.LazyLinear(out_features=output_features))
             case Symbol.RELU:
