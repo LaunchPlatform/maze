d6c8daa6b3f4d9068249eb474ec2a30c70754b6c
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-27 22:30:55 -0800

    add branch start

--- maze/gene/builder.py
+++ maze/gene/builder.py
@@ -6,24 +6,29 @@ from .symbols import Symbol
 
 
 def build_models(
-    symbols: typing.Iterator[Symbol], till_symbol: Symbol | None = None
-) -> list[nn.Module]:
+    symbols: typing.Iterator[Symbol],
+    till_symbols: typing.Container[Symbol] | None = None,
+) -> list[nn.Module] | tuple[list[nn.Module], Symbol]:
     modules = []
     while True:
         try:
             item = next(symbols)
         except StopIteration:
             break
-        if till_symbol is not None and item == till_symbol:
-            return modules
+        if till_symbols is not None and item in till_symbols:
+            return modules, item
         match item:
             case (Symbol.REPEAT_START, times):
+                repeating_modules, _ = build_models(
+                    symbols=symbols, till_symbols=[Symbol.REPEAT_END]
+                )
                 for _ in range(times):
-                    modules.extend(
-                        build_models(symbols=symbols, till_symbol=Symbol.REPEAT_END)
-                    )
+                    modules.extend(repeating_modules)
             case (Symbol.LINEAR, output_features):
                 modules.append(nn.LazyLinear(out_features=output_features))
+            case Symbol.BRANCH_START:
+                # TODO:
+                pass
             case Symbol.RELU:
                 modules.append(nn.ReLU())
             case Symbol.LEAKY_RELU:
