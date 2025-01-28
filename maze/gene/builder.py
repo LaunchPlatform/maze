657c2875e95539d79f97d3c575914408b651aeab
Fang-Pen Lin <hello@fangpenlin.com>
2025-01-27 22:17:05 -0800

    Add build modules

--- maze/gene/builder.py
+++ maze/gene/builder.py
@@ -1,5 +1,20 @@
+from torch import nn
+
 from .symbols import Symbol
 
 
-def build_models(symbols: list[Symbol]):
-    pass
+def build_models(symbols: list[Symbol]) -> list[nn.Module]:
+    modules = []
+    for item in symbols:
+        match item:
+            case (Symbol.REPEAT_START, times):
+                pass
+            case (Symbol.LINEAR, output_features):
+                modules.append(nn.LazyLinear(out_features=output_features))
+            case Symbol.RELU:
+                modules.append(nn.ReLU())
+            case Symbol.LEAKY_RELU:
+                modules.append(nn.LeakyReLU())
+            case Symbol.TANH:
+                modules.append(nn.Tanh())
+    return modules
