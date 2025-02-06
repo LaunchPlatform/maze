9969b0b7c9c2e9c01d7d86dc61ba98a0e145d903
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-06 01:41:51 -0800

    plot

---
+++ plot_model.py
@@ -0,0 +1,51 @@
+import json
+
+import matplotlib.pyplot as plt
+
+fig, ax1 = plt.subplots()
+
+accuracy_vals = []
+loss_vals = []
+with (
+    open("random_model.json", "rt") as random_fo,
+    open("handcraft_model0.json", "rt") as handcraft_fo,
+    open("handcraft_model_with_maxpool_upscale.json", "rt") as upscale_fo,
+):
+    for random_line, handcraft_line, upscale_line in zip(
+        random_fo.readlines(),
+        handcraft_fo.readlines(),
+        upscale_fo.readlines(),
+    ):
+        random_row = json.loads(random_line)
+        handcraft_row = json.loads(handcraft_line)
+        upscale_row = json.loads(upscale_line)
+
+        accuracy_vals.append(
+            (
+                random_row["accuracy"],
+                handcraft_row["accuracy"],
+                upscale_row["accuracy"],
+            )
+        )
+        loss_vals.append(
+            (
+                random_row["loss"],
+                handcraft_row["loss"],
+                upscale_row["loss"],
+            )
+        )
+
+ax1.set_xlabel("Epoch")
+ax1.set_ylabel("Accuracy")
+ax1.plot(accuracy_vals)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second Axes that shares the same x-axis
+
+ax2.set_ylabel("Loss")  # we already handled the x-label with ax1
+ax2.plot(loss_vals, linestyle="--")
+ax2.tick_params(axis="y")
+ax2.legend(["Random", "Handcraft", "MaxPoolUpscale"])
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+plt.show()
