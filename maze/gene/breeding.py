80533b1b196c4d77dd32b6e9a0e01fab19b14159
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-08 21:53:10 -0800

    Support bool for merge parameter as well

--- maze/gene/breeding.py
+++ maze/gene/breeding.py
@@ -5,22 +5,31 @@ from .symbols import BaseSymbol
 from .symbols import SimpleSymbol
 
 
+ParameterType = int | float | bool
+
+
 def merge_parameter(
-    lhs: int | float, rhs: int | float, jiter: int | float
-) -> int | float:
+    lhs: ParameterType,
+    rhs: ParameterType,
+    jiter: ParameterType | None = None,
+) -> ParameterType:
     """Merge parameters value between parent's parameter value randomly with a jiter range"""
     if type(lhs) is not type(rhs):
         raise ValueError(
             f"Expected the same type for lhs and rhs but got {type(lhs)} and {type(rhs)}"
         )
     if isinstance(lhs, int):
+        jiter = jiter or 0
         start = min(lhs, rhs) - jiter
         stop = max(lhs, rhs) + 1 + jiter
         return random.randrange(start, stop)
     elif isinstance(lhs, float):
+        jiter = jiter or 0.0
         start = min(lhs, rhs) - jiter
         stop = max(lhs, rhs) + jiter
         return random.uniform(start, stop)
+    elif isinstance(lhs, bool):
+        return random.choice(lhs, rhs)
     else:
         raise TypeError(f"Unexpected value type {type(lhs)}")
 
