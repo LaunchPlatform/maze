d2c3e9121bf2863cdf89514658efbbe238da2ac3
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-09 11:02:43 -0800

    Add mutation and tests

--- maze/gene/mutation.py
+++ maze/gene/mutation.py
@@ -1,18 +1,14 @@
-import dataclasses
 import enum
 import random
 
+from numpy.random import binomial
+
 
 @enum.unique
-class MutationType:
+class MutationType(enum.Enum):
     DELETE = "DELETE"
     DUPLICATE = "DUPLICATE"
     REVERSE = "REVERSE"
-
-
-@dataclasses.dataclass
-class MutationProbabilities:
-    pass
 
 
 def decide_mutations(
@@ -20,7 +16,7 @@ def decide_mutations(
 ) -> list[MutationType]:
     mutations = []
     for mutation_type, probability in probabilities.items():
-        occurrence = random.binomialvariate(probability, gene_length)
+        occurrence = binomial(gene_length, probability)
         if occurrence:
             mutations.extend([mutation_type] * occurrence)
     random.shuffle(mutations)
---
+++ tests/unit/gene/test_mutation.py
@@ -0,0 +1,53 @@
+import collections
+from collections import Counter
+
+import pytest
+
+from maze.gene.mutation import decide_mutations
+from maze.gene.mutation import MutationType
+
+
+@pytest.mark.parametrize(
+    "probabilities, length, expected",
+    [
+        (
+            {
+                MutationType.DELETE: 1,
+                MutationType.DUPLICATE: 1,
+                MutationType.REVERSE: 1,
+            },
+            1,
+            {
+                MutationType.DELETE: 1,
+                MutationType.DUPLICATE: 1,
+                MutationType.REVERSE: 1,
+            },
+        ),
+        (
+            {
+                MutationType.DELETE: 0.1,
+                MutationType.DUPLICATE: 0.2,
+                MutationType.REVERSE: 0.3,
+            },
+            10000,
+            {
+                MutationType.DELETE: 10000 * 0.1,
+                MutationType.DUPLICATE: 10000 * 0.2,
+                MutationType.REVERSE: 10000 * 0.3,
+            },
+        ),
+    ],
+)
+def test_decide_mutations(
+    probabilities: dict[MutationType, float], length: int, expected: list
+):
+    total_count = collections.defaultdict(int)
+    trial_count = 1000
+    for _ in range(trial_count):
+        result = decide_mutations(probabilities=probabilities, gene_length=length)
+        counter = Counter(result)
+        for key, value in counter.items():
+            total_count[key] += value
+    for key, value in total_count.items():
+        total_count[key] /= trial_count
+        assert total_count[key] == pytest.approx(expected[key], rel=10)
