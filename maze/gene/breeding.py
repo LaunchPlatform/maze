553d464065d485077059a527ec7e0f111ce0aadc
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-08 22:13:29 -0800

    Break down merge parameter

--- maze/gene/breeding.py
+++ maze/gene/breeding.py
@@ -2,36 +2,35 @@ import itertools
 import random
 
 from .symbols import BaseSymbol
+from .symbols import LinearSymbol
 from .symbols import SimpleSymbol
 
 
-ParameterType = int | float | bool
+def merge_bool(lhs: bool, rhs: bool) -> bool:
+    return random.choice((lhs, rhs))
 
 
-def merge_parameter(
-    lhs: ParameterType,
-    rhs: ParameterType,
-    jiter: ParameterType | None = None,
-) -> ParameterType:
-    """Merge parameters value between parent's parameter value randomly with a jiter range"""
-    if type(lhs) is not type(rhs):
-        raise ValueError(
-            f"Expected the same type for lhs and rhs but got {type(lhs)} and {type(rhs)}"
-        )
-    if isinstance(lhs, int):
-        jiter = jiter or 0
-        start = min(lhs, rhs) - jiter
-        stop = max(lhs, rhs) + 1 + jiter
-        return random.randrange(start, stop)
-    elif isinstance(lhs, float):
-        jiter = jiter or 0.0
-        start = min(lhs, rhs) - jiter
-        stop = max(lhs, rhs) + jiter
-        return random.uniform(start, stop)
-    elif isinstance(lhs, bool):
-        return random.choice(lhs, rhs)
-    else:
-        raise TypeError(f"Unexpected value type {type(lhs)}")
+def merge_int(lhs: int, rhs: int, jiter: int) -> int:
+    jiter = jiter
+    start = min(lhs, rhs) - jiter
+    stop = max(lhs, rhs) + 1 + jiter
+    return random.randrange(start, stop)
+
+
+def merge_float(lhs: float, rhs: float, jiter: float) -> float:
+    jiter = jiter or 0.0
+    start = min(lhs, rhs) - jiter
+    stop = max(lhs, rhs) + jiter
+    return random.uniform(start, stop)
+
+
+def merge_liner(lhs: LinearSymbol, rhs: LinearSymbol, out_features_jiter: int):
+    return LinearSymbol(
+        bias=merge_bool(lhs.bias, rhs.bias),
+        out_features=merge_int(
+            lhs.out_features, rhs.out_features, jiter=out_features_jiter
+        ),
+    )
 
 
 def merge(lhs: list[BaseSymbol], rhs: list[BaseSymbol]):
