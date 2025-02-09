b7e33a5808402fa3b739bd4e5097c05af367e33f
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-08 21:50:00 -0800

    Add merge param

--- maze/gene/breeding.py
+++ maze/gene/breeding.py
@@ -5,6 +5,26 @@ from .symbols import BaseSymbol
 from .symbols import SimpleSymbol
 
 
+def merge_parameter(
+    lhs: int | float, rhs: int | float, jiter: int | float
+) -> int | float:
+    """Merge parameters value between parent's parameter value randomly with a jiter range"""
+    if type(lhs) is not type(rhs):
+        raise ValueError(
+            f"Expected the same type for lhs and rhs but got {type(lhs)} and {type(rhs)}"
+        )
+    if isinstance(lhs, int):
+        start = min(lhs, rhs) - jiter
+        stop = max(lhs, rhs) + 1 + jiter
+        return random.randrange(start, stop)
+    elif isinstance(lhs, float):
+        start = min(lhs, rhs) - jiter
+        stop = max(lhs, rhs) + jiter
+        return random.uniform(start, stop)
+    else:
+        raise TypeError(f"Unexpected value type {type(lhs)}")
+
+
 def merge(lhs: list[BaseSymbol], rhs: list[BaseSymbol]):
     """Merge two symbol lists randomly"""
     for lhs_symbol, rhs_symbol in itertools.zip_longest(lhs, rhs):
@@ -18,7 +38,7 @@ def merge(lhs: list[BaseSymbol], rhs: list[BaseSymbol]):
             if isinstance(lhs_symbol, SimpleSymbol):
                 yield lhs_symbol
             else:
-                # merge parameters
+                # merge symbol with parameters
                 pass
         else:
             yield random.choice((lhs_symbol, rhs_symbol))
