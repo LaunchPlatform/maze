94a6cda1eb4b1228d45881f552c8e727de250852
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-10 20:55:37 -0800

    Add datasets

--- examples/king_of_mnist/main.py
+++ examples/king_of_mnist/main.py
@@ -1,5 +1,31 @@
+from sqlalchemy.orm import object_session
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
 from maze import models
 from maze.environment.templates import LinearEnvironment
+from maze.environment.vehicle import Vehicle
+from maze.environment.zone import eval_agent
+
+training_data = datasets.MNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
+)
+
+# Download test data from open datasets.
+test_data = datasets.MNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+batch_size = 64
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
 
 class KingOfMnist(LinearEnvironment):
