7e5f30115523b18b7312317e8fc4862070b05116
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-10 20:02:02 -0800

    Add the API

--- maze/environment/templates.py
+++ maze/environment/templates.py
@@ -6,7 +6,25 @@ class LinearEnvironment:
     group: str
 
     def name(self, index: int) -> str:
+        """Called to make the name of the environment for the given index
+
+        :param index: index of the environment
+        :return: name of the environment
+        """
         raise NotImplementedError
 
     def make_zones(self, index: int) -> list[models.Zone]:
+        """Called to initialize the zones for then environment of the given index. Only called once when we initialize
+        the database record for the environment.
+
+        :param index: index of the environment
+        :return: a list of Zones
+        """
+        raise NotImplementedError
+
+    def run_avatar(self, avatar: models.Avatar):
+        """Called to run an avatar (agent in a zone)
+
+        :param avatar: avatar to run
+        """
         raise NotImplementedError
