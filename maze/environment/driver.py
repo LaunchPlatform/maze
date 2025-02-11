6d64630e02cdb8275c6be1cda55f4f03f8a31f0c
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-10 21:03:37 -0800

    Add driver for initializing database

---
+++ maze/environment/driver.py
@@ -0,0 +1,23 @@
+import logging
+
+from ..db.session import Session
+from .templates import EnvironmentTemplate
+
+logger = logging.getLogger(__name__)
+
+
+class Driver:
+    def __init__(self, template: EnvironmentTemplate):
+        self.template = template
+
+    def initialize_db(self):
+        logger.info(
+            "Initializing db for template %s ...", self.template.__class__.__name__
+        )
+        with Session() as db:
+            if self.template.is_initialized(db):
+                logger.info("Already initialized, skip")
+                return
+            environments = self.template.make_environments()
+            for environment in environments:
+                db.add(environment)
