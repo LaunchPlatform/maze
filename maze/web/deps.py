4e10c96928d1543401765684f67de0cce0b2c07e
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-12 19:07:43 -0800

    List exps

--- maze/web/deps.py
+++ maze/web/deps.py
@@ -4,6 +4,7 @@ from fastapi import Depends
 from fastapi import Request
 from starlette.templating import Jinja2Templates
 
+from .. import models
 from ..core import constants
 from ..core.config import settings
 from ..db.session import Session
@@ -14,12 +15,11 @@ def get_db() -> typing.Generator[Session, None, None]:
         yield db
 
 
-def get_templates(
-    request: Request,
-) -> Jinja2Templates:
+def get_templates(request: Request, db: Session = Depends(get_db)) -> Jinja2Templates:
     templates = Jinja2Templates(directory=constants.PACKAGE_DIR / "web" / "templates")
     templates.env.globals["request"] = request
     templates.env.globals["settings"] = settings
+    templates.env.globals["experiments"] = db.query(models.Experiment)
     return templates
 
 
