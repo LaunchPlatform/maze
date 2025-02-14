15e9481b4cc49c451f1ef3ecb171b6fec9281f39
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-14 15:01:51 -0800

    Add pytorch pipline builder

--- maze/environment/torch_pipeline.py
+++ maze/environment/torch_pipeline.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+from ..gene import pipeline
+
 
 class Joint(nn.Module):
     def __init__(self, branch_modules: list[nn.Module]):
@@ -24,3 +26,33 @@ class Reshape(nn.Module):
 
     def forward(self, x):
         return x.view(x.size(0), *self.shape)
+
+
+def build_module(module: pipeline.Module) -> nn.Module:
+    match module:
+        case pipeline.ReLU:
+            return nn.ReLU()
+        case pipeline.LeakyReLU:
+            return nn.LeakyReLU()
+        case pipeline.Tanh:
+            return nn.Tanh()
+        case pipeline.Softmax:
+            return nn.Softmax()
+        case pipeline.Flatten:
+            return nn.Flatten()
+        case pipeline.Reshape(output_shape):
+            return Reshape(output_shape)
+        case pipeline.Linear(in_features, out_features, bias):
+            return nn.Linear(
+                bias=bias, in_features=in_features, out_features=out_features
+            )
+        case pipeline.AdaptiveMaxPool1d(out_features):
+            return nn.AdaptiveMaxPool1d(out_features)
+        case pipeline.AdaptiveAvgPool1d(out_features):
+            return nn.AdaptiveAvgPool1d(out_features)
+        case pipeline.Sequential(modules):
+            return nn.Sequential(*map(build_module, modules))
+        case pipeline.Joint(branches):
+            return Joint(branch_modules=list(map(build_module, branches)))
+        case _:
+            raise ValueError(f"Unknown module type {type(module)}")
