00b04c9ff30f17298e19c89705978132f548506b
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-13 17:16:31 -0800

    Add progress for env

--- maze/models/experiment.py
+++ maze/models/experiment.py
@@ -1,17 +1,61 @@
 import datetime
+import functools
+import typing
 import uuid
 
+from sqlalchemy import and_
 from sqlalchemy import DateTime
 from sqlalchemy import func
+from sqlalchemy import select
 from sqlalchemy import String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import aliased
 from sqlalchemy.orm import DynamicMapped
+from sqlalchemy.orm import foreign
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import remote
 
 from ..db.base import Base
 from .helpers import make_repr_attrs
+
+
+@functools.cache
+def current_period_subquery() -> typing.Any:
+    from .period import Period
+
+    max_period = aliased(Period)
+    experiment_id_max_period = (
+        select(
+            max_period.experiment_id, func.max(max_period.index).label("latest_index")
+        )
+        .select_from(max_period)
+        .group_by(max_period.experiment_id)
+    ).subquery(name="experiment_id_max_period")
+
+    period = aliased(Period)
+    return (
+        select(period).join(
+            experiment_id_max_period,
+            and_(
+                period.experiment_id == experiment_id_max_period.c.experiment_id,
+                period.index == experiment_id_max_period.c.latest_index,
+            ),
+        )
+    ).subquery(name="experiment_id_max_period")
+
+
+@functools.cache
+def current_period_alias() -> typing.Any:
+    from .period import Period
+
+    return aliased(Period, current_period_subquery())
+
+
+@functools.cache
+def current_period_primaryjoin() -> typing.Any:
+    return foreign(Experiment.id) == remote(current_period_alias().experiment_id)
 
 
 class Experiment(Base):
@@ -32,6 +76,12 @@ class Experiment(Base):
         "Period",
         back_populates="experiment",
         order_by="Period.index",
+    )
+    current_period: Mapped["Period"] = relationship(
+        current_period_alias,
+        primaryjoin=current_period_primaryjoin,
+        uselist=False,
+        viewonly=True,
     )
 
     def __repr__(self) -> str:
--- maze/web/views/environment.py
+++ maze/web/views/environment.py
@@ -4,6 +4,9 @@ from fastapi import APIRouter
 from fastapi import Request
 from fastapi import status
 from fastapi.exceptions import HTTPException
+from sqlalchemy import and_
+from sqlalchemy import case
+from sqlalchemy import func
 
 from .. import deps
 from ... import models
@@ -21,10 +24,40 @@ def view_environment(
     environment = db.get(models.Environment, id)
     if environment is None:
         return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+    current_period = environment.experiment.current_period
+    alive_avatars_column = func.sum(
+        case(
+            (models.Avatar.status == models.AvatarStatus.ALIVE, 1),
+            else_=0,
+        )
+    )
+    dead_avatars_column = func.sum(
+        case(
+            (models.Avatar.status != models.AvatarStatus.ALIVE, 1),
+            else_=0,
+        )
+    )
+
+    alive_avatars, dead_avatars = (
+        db.query(alive_avatars_column, dead_avatars_column)
+        .select_from(models.Avatar)
+        .join(
+            models.Zone,
+            and_(
+                models.Avatar.zone_id == models.Zone.id,
+                models.Zone.environment_id == environment.id,
+            ),
+        )
+        .filter(models.Avatar.period == current_period)
+    ).one()
+
     return templates.TemplateResponse(
         "environment/view_environment.html",
         dict(
             request=request,
             environment=environment,
+            alive_avatars=alive_avatars,
+            dead_avatars=dead_avatars,
         ),
     )
