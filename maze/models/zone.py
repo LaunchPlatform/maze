bacb82250115a94dbc6ae23c845e521e88610611
Fang-Pen Lin <hello@fangpenlin.com>
2025-02-13 17:54:06 -0800

    Add current_alive_avatars for zone

--- maze/models/zone.py
+++ maze/models/zone.py
@@ -1,13 +1,17 @@
 import datetime
 import uuid
 
+from sqlalchemy import and_
 from sqlalchemy import Boolean
 from sqlalchemy import DateTime
 from sqlalchemy import ForeignKey
 from sqlalchemy import func
 from sqlalchemy import Integer
+from sqlalchemy import select
 from sqlalchemy import UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import aliased
+from sqlalchemy.orm import column_property
 from sqlalchemy.orm import DynamicMapped
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
@@ -65,3 +69,71 @@ class Zone(Base):
     @property
     def display_name(self) -> str:
         return f"{self.environment.name}.zones[{self.index}]"
+
+    @classmethod
+    def __declare_last__(cls):
+        from .avatar import Avatar
+        from .avatar import AvatarStatus
+        from .period import Period
+        from .environment import Environment
+        from .experiment import Experiment
+
+        max_period = aliased(Period)
+        experiment_id_max_period = (
+            select(
+                max_period.experiment_id,
+                func.max(max_period.index).label("latest_index"),
+            )
+            .select_from(max_period)
+            .group_by(max_period.experiment_id)
+        ).subquery(name="experiment_id_max_period")
+
+        aliased_period = aliased(Period)
+        aliased_environment = aliased(Environment)
+        aliased_experiment = aliased(Experiment)
+        aliased_zone = aliased(Zone)
+        current_period_id = (
+            select(aliased_period.id)
+            .select_from(aliased_period)
+            .join(
+                experiment_id_max_period,
+                and_(
+                    aliased_period.experiment_id
+                    == experiment_id_max_period.c.experiment_id,
+                    aliased_period.index == experiment_id_max_period.c.latest_index,
+                ),
+            )
+            .join(
+                aliased_experiment,
+                aliased_period.experiment_id == aliased_experiment.id,
+            )
+            .join(
+                aliased_environment,
+                aliased_environment.experiment_id == aliased_experiment.id,
+            )
+            .join(
+                aliased_zone,
+                and_(
+                    aliased_zone.environment_id == aliased_environment.id,
+                    aliased_zone.id == cls.id,
+                ),
+            )
+        ).scalar_subquery()
+
+        aliased_avatar = aliased(Avatar)
+        alive_avatars = (
+            select(func.count())
+            .select_from(aliased_avatar)
+            .where(
+                and_(
+                    aliased_avatar.zone_id == cls.id,
+                    aliased_avatar.period_id == current_period_id,
+                    aliased_avatar.status == AvatarStatus.ALIVE,
+                )
+            )
+        )
+
+        cls.current_alive_avatars = column_property(
+            alive_avatars,
+            deferred=True,
+        )
