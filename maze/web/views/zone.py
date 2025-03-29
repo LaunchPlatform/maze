fd421a301b63f8049f8eb13742f459cbca53b572
Fang-Pen Lin <hello@fangpenlin.com>
2025-03-28 21:00:32 -0700

    Fix n+1 issue

--- maze/web/views/zone.py
+++ maze/web/views/zone.py
@@ -10,7 +10,8 @@ from sqlalchemy import and_
 from sqlalchemy import func
 from sqlalchemy import select
 from sqlalchemy.orm import aliased
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import undefer
 
 from .. import deps
 from ... import models
@@ -65,11 +66,7 @@ def view_zone(
     period_avatars_query = (
         db.query(models.Period, alive_avatar_count, dead_avatar_count, models.Avatar)
         .select_from(models.Period)
-        .options(
-            selectinload(models.Period.avatars)
-            .undefer(models.Avatar.credit)
-            .selectinload(models.Avatar.agent)
-        )
+        .options(undefer(models.Avatar.credit), joinedload(models.Avatar.agent))
         .join(models.Experiment, models.Period.experiment_id == models.Experiment.id)
         .join(
             models.Environment, models.Environment.experiment_id == models.Experiment.id
